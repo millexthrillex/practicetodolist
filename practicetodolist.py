c508962a93ddce1385a839e3040735b33e6abbf9
Terry Miller <millerterryt@gmail.com>
2022-10-20 14:57:35 -0400

    Initial commit with requirements and __main__ boilder plate

---
+++ practicetodolist.py
@@ -0,0 +1,17 @@
+# Requirements:
+# Text User Interface
+# Read Evaluate Print Loop
+# Add Tasks
+# Delete Tasks when complete
+# list current tasks in the order they were added
+# Persist across runs (load in multiple terminals) 
+# every user gets their own list
+# editable storage format
+# all tasks will be entered by the user
+# report # of tasks 
+
+
+
+
+if __name__ == "__main__":
+    print("Do something")
